28f9e46afb3bd6f882e0c0c07b4363ac1bade816
DESKTOP-36EQGFC\HP <mmlee0819@gmail.com>
2024-07-28 20:17:59 +0800

    Remove useless files

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,6 @@
 /** @type { import('@storybook/react').Preview } */
+import '../src/assets/index.css'
+
 const preview = {
   parameters: {
     controls: {
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { Button } from "./Button/Button";
+import { Header } from "./Header/Header";
+import { Page } from "./Page/Page";
+
+export { Button, Header, Page };
