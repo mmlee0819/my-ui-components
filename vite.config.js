7cf73c8e214e321b125ee8b38c43731c14c29740
DESKTOP-36EQGFC\HP <mmlee0819@gmail.com>
2024-07-28 21:28:51 +0800

    Publish v0.0.1

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,19 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: 'src/components/index.js',
+      output: {
+        dir: 'dist',
+        format: 'es',
+      },
+    },
+    lib: {
+      entry: 'src/components/index.js',
+      name: '', 
+      formats: ['es', 'cjs'], 
+      fileName: (format) => `my-ui-components.${format}.js` 
+    },
+  },
 })
